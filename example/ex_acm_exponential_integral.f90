1ce2ba1a4bcf22d6759c181ccaa26cc3543bff69
rodpcastro <rodpcastro18@gmail.com>
2025-05-27 01:37:35 -0300

    revamped exmaples

--- example/ex_acm_exponential_integral.f90
+++ example/ex_acm_exponential_integral.f90
@@ -1,5 +1,5 @@
-program specfun
-! Simple test of special functions.
+program example_acm_exponential_integral
+! Simple test exponential integrals Ei and E1.
 !
 ! Author
 ! ------
@@ -7,16 +7,21 @@ program specfun
 !
 ! History
 ! -------
-! 25-04-2025 - Rodrigo Castro - Original code
+! 27-04-2025 - Rodrigo Castro - Original code
 
   use, intrinsic :: iso_fortran_env, only: real64
-  use acm_exponential_integral, only: e1
+  use acm_exponential_integral, only: ei, e1
 
   implicit none
   
   character(:), allocatable :: fmt
-  real(real64) :: e1x_t1, e1x_t2, e1z_t1, e1z_t2, e1z_t3
+  real(real64) :: ei_t1, ei_t2, e1x_t1, e1x_t2, &
+                  e1z_t1, e1z_t2, e1z_t3, ei_root
 
+  ei_root = 0.3725074107813666d0
+
+  ei_t1 = abs(-0.30266853926582593d0 - ei(0.3d0)) / abs(-0.30266853926582593d0)
+  ei_t2 = abs(1.058563689713169d+20 - ei(50.0d0)) / abs(1.058563689713169d+20)
   e1x_t1 = abs(0.55977359477616084d0 - e1(0.5d0)) / abs(0.55977359477616084d0)
   e1x_t2 = abs(0.10001958240663265d0 - e1(1.5d0)) / abs(0.10001958240663265d0)
   e1z_t1 = abs((-0.014529959529202443d0, -0.015866824826503003d0) - e1((2.5d0, 1.8d0))) / &
@@ -30,15 +35,20 @@ program specfun
   print '(a)', '---------------------'
   print '(a)', 'Exponential Integrals'
   print '(a)', '---------------------'
+  print fmt, 'ei_t1  = ', ei_t1
+  print fmt, 'ei_t2  = ', ei_t2
   print fmt, 'e1x_t1 = ', e1x_t1
   print fmt, 'e1x_t2 = ', e1x_t2
   print fmt, 'e1z_t1 = ', e1z_t1
   print fmt, 'e1z_t2 = ', e1z_t2
   print fmt, 'e1z_t3 = ', e1z_t3
+  print '(a, sp, g0)', 'ei(0.0)  = ', ei(0.0d0)
   print '(a, sp, g0)', 'e1x(0.0) = ', e1(0.0d0)
   print '(a, sp, g0, f4.1)', 'e1z(0.0) = ', e1((0.0d0, 0.0d0))
+  print fmt, '-ei(1.0)  = ', -ei(1.0d0)
   print '(a, 2(es22.15, 1x))', 'e1z(-1.0) = ', e1((-1.0d0, 0.0d0))
+  print fmt, 'ei(-1.5)   = ', ei(-1.5d0)
   print fmt, '-e1x(1.5)  = ', -e1(1.5d0)
+  print fmt, 'ei(root) = ', ei(ei_root)
 
-end program specfun
-
+end program example_acm_exponential_integral
