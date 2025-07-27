0a6a95a963fc4917673553988613f3711febe4ba
rodpcastro <rodpcastro18@gmail.com>
2025-07-27 13:25:33 -0300

    removed unnecessary examples

--- example/main.f90
+++ example/main.f90
@@ -1,25 +1,23 @@
 program examples
 ! Simple tests for internal procedures and special functions.
 
-  use example_gauss_quadrature
-  use example_polygamma
   use example_exponential_integral
   use example_bessel
   use example_hypergeometric
+  use example_struve
 
   implicit none
   
-  ! call example_g8()
-  ! call example_gaus8()
-  ! call example_psixn()
   call example_ei()
   call example_e1x()
   call example_e1z()
-  ! call example_enz()
+  call example_enz()
   call example_j0x()
   call example_j1x()
   call example_y0x()
   call example_y1x()
   call example_hyp2f1()
+  call example_struveh0()
+  call example_struveh1()
 
 end program examples
